04f4b6855799a1a5e511783bfed16cf86f6fb350
gregothedeveloper <gregoforty2@gmail.com>
2024-07-04 15:49:52 +0300

    performing operations and printing to the terminal

---
+++ trial.py
@@ -0,0 +1,24 @@
+number1 = float(input('Enter the first number: '))
+number2 = float(input('Enter the first number: '))
+
+# perform Addition
+addition = number1 + number2
+
+# perform subtraction
+subtraction = number1 - number2
+
+# perform multiplication
+multiplication = number1 * number2
+
+
+# perform division
+if number2 != 0:
+    division = number1 / number2
+else:
+    division = "undefined (cannot divide by zero)"
+
+# print section
+print(f'The result of {number1} + {number2} is {addition}')
+print(f'The result of {number1} - {number2} is {subtraction}')
+print(f'The result of {number1} * {number2} is {multiplication}')
+print(f'The result of {number1} / {number2} is {division}')
