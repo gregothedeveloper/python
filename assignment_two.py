56acf62d1d03bd09c370f78a0f5f6356a0df5061
gregothedeveloper <gregoforty2@gmail.com>
2024-07-07 04:27:44 +0300

    Additional features

--- assignment_two.py
+++ assignment_two.py
@@ -1,36 +1,49 @@
 balance = 10000
 
+# Menu display
 while True:
     print("\nMenu:")
     print("1. Check balance")
     print("2. Deposit money")
     print("3. Withdraw money")
     print("4. Exit")
-    
+
+# Handling customers' choices
+# Handling customers' balance choice
+
     choice = input("Enter your choice (1-4): ")
     
     if choice == '1':
         print(f"Your current balance is: Ksh.{balance}")
-        break
 
-    
+# Handling customers' deposit choice
+
     elif choice == '2':
-        deposit_amount = float(input("Enter amount: Ksh. "))
-        balance += deposit_amount
-        print(f"You have successfully deposited Ksh.{deposit_amount}. Your new balance is: Ksh.{balance}. Thank you for using our service")
-        break
-    
+        deposit_amount = float(input("Enter amount to deposit: Ksh. "))
+        if deposit_amount > 0:
+            balance += deposit_amount
+            print(f"Congratulations!, You have successfully deposited Ksh.{deposit_amount}. Your new balance is: Ksh.{balance}. Thank you for using our service.")
+        else:
+            print("Ooops!, Invalid deposit amount. Please enter a positive value.")
+
+# Handling customers' withdrawal choice
+
     elif choice == '3':
         withdraw_amount = float(input("Enter amount to withdraw: Ksh. "))
-        if withdraw_amount <= balance:
-            balance -= withdraw_amount
-            print(f"You have successfully withdrawn Ksh.{withdraw_amount}. Your new balance is: Ksh.{balance}. Thank you for using our service")
+        if withdraw_amount > 0:
+            if withdraw_amount <= balance:
+                balance -= withdraw_amount
+                print(f"Congratulations!, You have successfully withdrawn Ksh.{withdraw_amount}. Your new balance is: Ksh.{balance}. Thank you for using our service.")
+            else:
+                print("Ooops!, Insufficient balance. Withdrawal denied.")
         else:
-            print("Insufficient balance. Withdrawal denied.")
-        break
-    
+            print("Invalid withdrawal amount. Please enter a positive value.")
+
+    # Handling customers'  exit choice
+
     elif choice == '4':
         print("Thank you for using our service. Goodbye!")
         break
+
     else:
         print("Invalid choice. Please enter a number between 1 and 4.")
